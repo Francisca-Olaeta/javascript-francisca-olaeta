4f467cf78c8995666334cce748fdd9e40e224757
Francisca-Olaeta <francisca.olaeta@gmail.com>
2022-06-17 19:16:30 -0400

    currency

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,35 +4,13 @@ var vle = document.getElementById('price').getAttribute('value');
 var clr = document.getElementById('color').value;
 
 
-
 function calculo () {
 var qty = document.getElementById('quantity').value;
 var vle = document.getElementById('price').getAttribute('value');
 var clr = document.getElementById('color').value;
 var result = qty * vle;
 
-document.getElementById('total').innerHTML = result.toLocaleString();
+document.getElementById('total').innerHTML = result.toLocaleString('es-CL', {style:'currency', currency: 'CLP'});
 document.getElementById('finalQuantity').innerHTML = qty;
 document.getElementById('chooseColor').style.backgroundColor = clr;
-
-
-
-
-
-console.log(clr);
 }
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
